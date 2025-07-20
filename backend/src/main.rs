5c357a5ad2d276f3675a824d69448385909f3949
LDM-A <larsdemaere@hotmail.com>
2025-07-20 12:30:41 +0200

    connections add & remove | send message to all connections

---
+++ backend/src/main.rs
@@ -0,0 +1,53 @@
+use std::{collections::HashMap, sync::atomic::{AtomicUsize, Ordering}};
+use rocket_ws::{Channel, Message, WebSocket,stream::DuplexStream};
+use rocket::{State, tokio::sync::Mutex};
+use rocket::futures::{SinkExt,stream::SplitSink, StreamExt};
+
+
+static USER_ID_COUNTER: AtomicUsize = AtomicUsize::new(1);
+
+#[derive(Default)]
+struct ChatRoom {
+    connections: Mutex<HashMap<usize,SplitSink<DuplexStream,Message>>> 
+}
+
+#[rocket::get("/")]
+fn chat<'r>(ws: WebSocket, state: &'r State<ChatRoom>) -> Channel<'r> {
+    ws.channel(move |stream| Box::pin(async move {
+
+        let user_id = USER_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
+        let (ws_sink, mut ws_stream) = stream.split();
+        {
+            let mut conns = state.connections.lock().await;
+            conns.insert(user_id, ws_sink);
+        }
+        while let Some(message) = ws_stream.next().await {
+            
+            {
+                let mut conns = state.connections.lock().await;
+                let msg = message?;
+                for (_id, sink) in conns.iter_mut() {
+                    let _ = sink.send(msg.clone()).await;
+                }
+            }
+        }
+
+        {
+            let mut conns = state.connections.lock().await;
+            conns.remove(&user_id);
+        }
+
+        Ok(())
+    }))
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+    .mount("/", rocket::routes![
+        chat
+    ])
+    .manage(ChatRoom::default())
+    .launch()
+    .await;
+}
