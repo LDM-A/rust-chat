ba1ec632b127b43a6efb2d482a6e9eaa3bae44b9
LDM-A <larsdemaere@hotmail.com>
2025-09-06 08:07:17 +0200

    userlist broadcast message

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, sync::atomic::{AtomicUsize, Ordering}};
 use chrono::Utc;
-use common::{ChatMessage, WebSocketMessage};
+use common::*;
 use rocket_ws::{Channel, Message, WebSocket,stream::DuplexStream};
 use rocket::{State, tokio::sync::Mutex};
 use rocket::futures::{SinkExt,stream::SplitSink, StreamExt};
@@ -32,7 +32,7 @@ impl ChatRoom {
             created_at: Utc::now().naive_utc(),
         };
         let websocket_message = WebSocketMessage {
-            message_type: common::WebSocketMessageType::NewMessage,
+            message_type: WebSocketMessageType::NewMessage,
             message: Some(chat_message),
             users: None,
         };
@@ -42,6 +42,29 @@ impl ChatRoom {
                 Message::Text(json!(websocket_message).to_string())
             ).await;
         }
+    }
+
+    pub async fn broadcast_user_list(&self){
+        let mut conns = self.connections.lock().await;
+        let mut users = vec![];
+
+        for (id, _) in conns.iter() {
+            users.push(format!("User #{}",id));
+        }
+
+        let websocket_message = WebSocketMessage {
+            message_type: WebSocketMessageType::UsersList,
+            message: None,
+            users: Some(users),
+        };
+
+        for (_id, sink) in conns.iter_mut() {
+            let _ = sink.send(
+                Message::Text(json!(websocket_message).to_string())
+            ).await;
+        }
+
+        
     }
 }
 
@@ -53,12 +76,14 @@ fn chat<'r>(ws: WebSocket, state: &'r State<ChatRoom>) -> Channel<'r> {
         let (ws_sink, mut ws_stream) = stream.split();
 
         state.add(user_id, ws_sink).await;
+        state.broadcast_user_list().await;
 
         while let Some(message) = ws_stream.next().await {
             state.broadcast_message(message?, user_id).await;
         }
         
         state.remove(user_id).await;
+        state.broadcast_user_list().await;
 
         Ok(())
     }))
